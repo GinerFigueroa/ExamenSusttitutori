5810a1d8acc8ddd43bbdd7b5e90f3c3a5efaa7f5
Giner Figueroa Gonzalez <2113110108@untels.edu.pe>
2024-07-24 12:23:43 -0500

    Figueroa - Notas en Columnas

---
+++ app/src/main/java/com/example/examen_sustitutorio_figueroa/NotasInteractionListener.java
@@ -0,0 +1,7 @@
+package com.example.examen_sustitutorio_figueroa;
+
+public interface NotasInteractionListener {
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+}
